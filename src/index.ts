74d0b3865abbd79dd3533902ff837f2c2f74b0f0
Ilya Shumchenko <Ilya.Shumchenko@noveogroup.com>
2023-02-01 22:54:44 +0300

    hotfix)

--- src/index.ts
+++ src/index.ts
@@ -23,16 +23,17 @@ let camera: THREE.PerspectiveCamera;
 
 const updateArr: (() => void)[] = [];
 
+const context = new AudioContext();
+let analyser: AnalyserNode;
+
 const num = 32;
-
-const Arr = new Uint8Array(num * 2);
-
-// let controls: any;
-// const clock = new THREE.Clock();
+const freqArray = new Uint8Array(num * 2);
 
 const { Scene, WebGLRenderer, PerspectiveCamera, DirectionalLight, AmbientLight } = THREE;
 
-async function init() {
+init();
+
+function init() {
     renderer = new WebGLRenderer();
     scene = new Scene();
 
@@ -43,14 +44,13 @@ async function init() {
     root.appendChild(renderer.domElement);
 
     window.addEventListener('resize', onWindowResize);
+    addAudioMusic();
 
     /////////
 
     addFloor();
-    await addLight();
+    addLight();
     addCube();
-
-    addAudioMusic();
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.target.copy(new THREE.Vector3(0, 30, 0));
@@ -66,14 +66,14 @@ function onWindowResize() {
 
 function addLight() {
     for (let i = -num; i < num; i++) {
-        const rectLight1 = new THREE.RectAreaLight(0x00ff00, 7, 4, 1);
+        const rectLight1 = new THREE.RectAreaLight(0x000000, 7, 4, 1);
         rectLight1.position.set(i * 4, -0.1, 50);
 
         scene.add(rectLight1);
         scene.add(new RectAreaLightHelper(rectLight1));
 
         updateArr.push(() => {
-            const height = Arr[Math.abs(i - num)];
+            const height = freqArray[Math.abs(i - num)];
             rectLight1.height = height;
             rectLight1.color.setRGB(height / 255, (255 - height) / 255, 0.9);
         });
@@ -92,68 +92,62 @@ function addFloor() {
 }
 
 function addCube() {
-    const texture = new THREE.TextureLoader().load(rg, (text) => {
-        text.magFilter = THREE.NearestFilter;
+    const texture = new THREE.TextureLoader().load(rg);
+    texture.magFilter = THREE.NearestFilter;
 
-        const cubeMaterialArray: THREE.MeshStandardMaterial[] = [];
+    const cubeMaterialArray: THREE.MeshStandardMaterial[] = [];
 
-        // order to add materials: x+,x-,y+,y-,z+,z-
-        cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
-        cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
-        cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
-        cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
-        cubeMaterialArray.push(new THREE.MeshStandardMaterial({ map: text, color: 0xffffff }));
-        cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    // order to add materials: x+,x-,y+,y-,z+,z-
+    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ map: texture, color: 0xffffff }));
+    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
 
-        const pos = new THREE.BoxGeometry(100, 100, 100);
-        const cube = new THREE.Mesh(pos, cubeMaterialArray);
-        scene.add(cube);
-        cube.position.set(0, 50, -200);
+    const pos = new THREE.BoxGeometry(100, 100, 100);
+    const cube = new THREE.Mesh(pos, cubeMaterialArray);
+    scene.add(cube);
+    cube.position.set(0, 50, -200);
+}
+
+function addAudioMusic() {
+    analyser = context.createAnalyser();
+
+    const music = new Audio(mus);
+    const music4An = new Audio(mus);
+
+    music4An.addEventListener('canplaythrough', () => {
+        context.resume();
+        const src = context.createMediaElementSource(music4An);
+        src.connect(analyser);
+
+        animate();
+    });
+
+    window.addEventListener('keyup', (key) => {
+        context.resume();
+        if (key.keyCode === 32) {
+            if (!music4An.paused) {
+                music.pause();
+                music4An.pause();
+            } else {
+                music.play();
+                music4An.play();
+            }
+        }
     });
 }
 
 function animate() {
+    render();
     requestAnimationFrame(animate);
 
-    render();
     stats.update();
 }
 
 function render() {
     renderer.render(scene, camera);
+    void analyser.getByteFrequencyData(freqArray);
     updateArr.forEach((func) => func());
-
-    analyser.getByteFrequencyData(Arr);
 }
-
-let analyser: AnalyserNode;
-
-function addAudioMusic() {
-    const context = new AudioContext();
-    analyser = context.createAnalyser();
-    analyser.fftSize = 2048;
-
-    const music = new Audio(mus);
-    const music4An = new Audio(mus);
-
-    music4An.addEventListener('loadeddata', () => {
-        const src = context.createMediaElementSource(music4An);
-        src.connect(analyser);
-
-        window.addEventListener('keyup', (key) => {
-            if (key.keyCode === 32) {
-                if (!music.paused) {
-                    music.pause();
-                    music4An.pause();
-                } else {
-                    music.play();
-                    music4An.play();
-                }
-            }
-        });
-    });
-}
-
-init().then(() => {
-    animate();
-});
