0eb89b4706a19300225c4cd716d3a5d62afed0ff
Ilya Shumchenko <Ilya.Shumchenko@noveogroup.com>
2023-02-03 00:54:57 +0300

    demo

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import * as Stats from 'stats.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js';
+import { Reflector } from 'three/examples/jsm/objects/Reflector.js';
 
 import mus from './RW-for-3js.mp3';
 import rg from './textures/rg.webp';
@@ -26,19 +27,27 @@ const updateArr: (() => void)[] = [];
 const context = new AudioContext();
 let analyser: AnalyserNode;
 
-const num = 32;
+const num = 64;
 const freqArray = new Uint8Array(num * 2);
 
-const { Scene, WebGLRenderer, PerspectiveCamera, DirectionalLight, AmbientLight } = THREE;
+const {
+    Scene,
+    WebGLRenderer,
+    PerspectiveCamera,
+    DirectionalLight,
+    AmbientLight,
+    BufferGeometry,
+    Float32BufferAttribute,
+} = THREE;
 
 init();
 
 function init() {
-    renderer = new WebGLRenderer();
+    renderer = new WebGLRenderer({ antialias: true, failIfMajorPerformanceCaveat: true, logarithmicDepthBuffer: true });
     scene = new Scene();
 
-    camera = new PerspectiveCamera(90, ASPECT, 0.1, 1000);
-    camera.position.set(0, 30, -20);
+    camera = new PerspectiveCamera(90, ASPECT, 0.1, 50000);
+    camera.position.set(800, 800, -2000);
 
     renderer.setSize(WIDTH, HEIGHT);
     root.appendChild(renderer.domElement);
@@ -48,13 +57,15 @@ function init() {
 
     /////////
 
-    addFloor();
+    // addFloor();
     addLight();
     addCube();
 
     const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.copy(new THREE.Vector3(0, 30, 0));
+    controls.target.copy(new THREE.Vector3(0, 0, 0));
     controls.update();
+
+    // animate();
 }
 
 function onWindowResize() {
@@ -65,22 +76,26 @@ function onWindowResize() {
 }
 
 function addLight() {
-    for (let i = -num; i < num; i++) {
-        const rectLight1 = new THREE.RectAreaLight(0xffffff, 15, 3, 10);
-        rectLight1.position.set(i * 4, -0.2, 50);
+    // for (let i = -num; i < num; i++) {
+    //     const rectLight1 = new THREE.RectAreaLight(0xffffff, 15, 3, 10);
+    //     rectLight1.position.set(i * 4, -0.2, 50);
 
-        scene.add(rectLight1);
-        scene.add(new RectAreaLightHelper(rectLight1));
+    //     scene.add(rectLight1);
+    //     scene.add(new RectAreaLightHelper(rectLight1));
 
-        updateArr.push(() => {
-            const height = freqArray[Math.abs(i - num)] || 0;
-            rectLight1.height = height;
-            rectLight1.color.setRGB(height / 255, (255 - height) / 255, 0.9);
-        });
-    }
+    //     updateArr.push(() => {
+    //         const height = freqArray[Math.abs(i - num)] || 0;
+    //         rectLight1.height = height;
+    //         rectLight1.color.setRGB(height / 255, (255 - height) / 255, 0.9);
+    //     });
+    // }
 
-    const am = new AmbientLight(0xffffff, 0.05);
+    const am = new AmbientLight(0xffffff, 1);
     scene.add(am);
+
+    // const dl = new DirectionalLight(0xffffff, 1);
+    // dl.position.set(1000, 1000, 1000);
+    // scene.add(dl);
 }
 
 function addFloor() {
@@ -91,28 +106,90 @@ function addFloor() {
     scene.add(mshStdFloor);
 }
 
+function updateBoxGeometry(
+    mesh: THREE.Mesh<THREE.BoxGeometry, THREE.MeshStandardMaterial>,
+    geometry: THREE.BoxGeometry,
+) {
+    mesh.geometry.dispose();
+
+    mesh.geometry = geometry;
+}
+
+function addMirror(xOffset: number, yOffset: number, zOffset: number, angleX: number, angleY: number, angleZ: number) {
+    const geometry = new THREE.PlaneGeometry(2000, 2000);
+    const verticalMirror = new Reflector(geometry, {
+        //clipBias: 0.003,
+        textureWidth: window.innerWidth * window.devicePixelRatio,
+        textureHeight: window.innerHeight * window.devicePixelRatio,
+        color: 0x61616a,
+    });
+    verticalMirror.position.x = xOffset;
+    verticalMirror.position.y = yOffset;
+    verticalMirror.position.z = zOffset;
+    verticalMirror.rotateX(angleX);
+    verticalMirror.rotateY(angleY);
+    verticalMirror.rotateZ(angleZ);
+    scene.add(verticalMirror);
+}
+
 function addCube() {
-    const texture = new THREE.TextureLoader().load(rg);
-    texture.magFilter = THREE.NearestFilter;
+    // const texture = new THREE.TextureLoader().load(rg);
+    // texture.magFilter = THREE.NearestFilter;
 
     const cubeMaterialArray: THREE.MeshStandardMaterial[] = [];
 
     // order to add materials: x+,x-,y+,y-,z+,z-
-    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
-    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
-    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
-    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
-    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ map: texture, color: 0xffffff }));
-    cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    // cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    // cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    // cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    // cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
+    // cubeMaterialArray.push(new THREE.MeshStandardMaterial({ map: texture, color: 0xffffff }));
+    // cubeMaterialArray.push(new THREE.MeshStandardMaterial({ color: 0xffffff, roughness: 0.1, metalness: 0.2 }));
 
-    const pos = new THREE.BoxGeometry(100, 100, 100);
-    const cube = new THREE.Mesh(pos, cubeMaterialArray);
-    scene.add(cube);
-    cube.position.set(0, 50, -200);
+    addMirror(0, 0, 1000, 0, Math.PI, 0);
+    addMirror(0, 0, -1000, 0, 0, 0);
+
+    addMirror(1000, 0, 0, 0, -Math.PI / 2, 0);
+    addMirror(-1000, 0, 0, 0, Math.PI / 2, 0);
+
+    const pos1 = new THREE.BoxGeometry(2002, 2002, 2002);
+    const mesh1 = new THREE.MeshStandardMaterial({
+        color: 0x1c1035,
+        side: THREE.BackSide,
+        metalness: 0,
+        roughness: 0,
+        transparent: true,
+        opacity: 0.8,
+    });
+    const cube1 = new THREE.Mesh(pos1, mesh1);
+    scene.add(cube1);
+
+    for (let i = -num + 1; i < num; i++) {
+        const size = 6;
+        const pos = new THREE.BoxGeometry(size, size, size);
+        const mesh = new THREE.MeshStandardMaterial({ color: 0xffffff });
+        const cube = new THREE.Mesh(pos, mesh);
+        scene.add(cube);
+        // cube.position.set(0, 50, -200);
+        cube.position.set(i * (size * 1.52), 0, 0);
+
+        updateArr.push(() => {
+            const height = freqArray[Math.abs(i - num)] || 0;
+            const red = height ? height / 255 : 0;
+            const green = height ? (255 - height) / 255 : 0;
+            const blue = height ? 0.9 : 0;
+            mesh.color.setRGB(red, green, blue);
+
+            // rectLight1.height = height;
+            // rectLight1.color.setRGB(height / 255, (255 - height) / 255, 0.9);
+            updateBoxGeometry(cube, new THREE.BoxGeometry(size, height * 2 + 0.5, size));
+        });
+    }
 }
 
 function addAudioMusic() {
     analyser = context.createAnalyser();
+    analyser.channelCount = 12;
 
     const music = new Audio(mus);
     const music4An = new Audio(mus);
@@ -140,10 +217,13 @@ function addAudioMusic() {
 }
 
 function animate() {
-    render();
-    requestAnimationFrame(animate);
+    setTimeout(() => {
+        render();
 
-    stats.update();
+        requestAnimationFrame(animate);
+
+        stats.update();
+    }, 60 / 1000);
 }
 
 function render() {
